ed1bb94bdcb20378af4e1e7bc4285a7085b117ac
Blokyk <eliot.courvoisier@gmail.com>
2019-04-03 22:04:51 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+using System.Linq;
+
+namespace cssparser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(Directory.GetCurrentDirectory() + "/res/simple.css").ToList();
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                var escaped = Escape(lines[i]);
+
+                var escapedArray = escaped.Split(';');
+
+                lines.RemoveAt(i);
+
+                foreach (var str in escapedArray)
+                {
+                    lines.Insert(i++, str);
+                }
+                
+
+            }
+
+            Tokenize(lines);
+        }
+
+        // See https://www.w3.org/TR/css-syntax-3/#tokenizer-algorithms for reference
+        static void Tokenize(string[] lines) {
+            var globalTokens = new List<Tokens[]>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                var lineTokens = new List<Tokens>();
+                for (int j = 0; j < line.Length; j++)
+                {
+                    var currToken = line[j];
+
+                    if (Char.IsWhiteSpace(currToken)) {
+                        lineTokens.Add(Tokens.whitespaceToken);
+                        continue;
+                    }
+
+                    if (currToken == '\u0022') {
+                        
+                    }
+                }
+            }
+        }
+
+        static (StringToken token, int offset) TokenizeString(string line) {
+            var token = new StringToken("");
+            int i = 0;
+
+            for (; i < line; i++)
+            {
+                if (line[i] == '\n') {
+                    token.token = Tokens.badStringToken;
+                    break;
+                }
+
+                if (line[i] == '\\')
+                {
+                    if (line[i+1] == '\n')
+                    {
+                        i++;
+                        continue;
+                    }
+                }
+            }
+
+            return (token, i);
+        }
+
+        static (char codePoint, bool success) TokenizeEscape(string line) {
+            if (line[0] != '\\')
+            {
+                return (Char.MinValue, false);
+            }
+
+            if (line[1] == '\n') {
+                return (Char.MinValue, false);
+            }
+
+            return (line[1], true);
+        }
+    }
+}
---
+++ Token.cs
@@ -0,0 +1,102 @@
+// See https://www.w3.org/TR/css-syntax-3/#tokenization for reference
+public enum Tokens {
+    identToken, functionToken, atToken, hashToken, stringToken, badStringToken,
+    urlToken, badUrlToken, delimToken, numberToken, percentToken, dimensionToken,
+    unicodeRangeToken, includeMatchToken, dashMatchToken, prefixMatchToken,
+    suffixMatchToken, substringMatchToken, columnToken, whitespaceToken, CDOToken,
+    CDCToken, colonToken, semicolonToken, commaToken, openSquareToken, closeSquareToken,
+    openParenToken, closeParenToken, openCurlyToken, closeCurlyToken
+}
+
+public class Token {
+    public char representation;
+
+    public Tokens token;
+
+    public Token(char codePoint, Tokens token) {
+        representation = codePoints;
+        this.token = token;
+    }
+}
+
+public class ComplexToken : Token {
+    public override string representation;
+
+    public ComplexToken(string codePoints, Tokens token) {
+        representation = codePoints;
+        this.token = token;
+    }
+}
+
+public class StringToken : ComplexToken {
+
+    public override Tokens token;
+    public StringToken(string codePoints) : base(codePoints, Tokens.stringToken) {}
+
+    public void Add(char representationChar) {
+        representation += representationChar;
+    }
+}
+
+public class UrlToken : ComplexToken {
+
+    public override Tokens token;
+    public StringToken url;
+
+    public UrlToken(string codePoints, StringToken url) : base(codePoints, Tokens.urlToken) {
+        this.url = url;
+    }
+}
+public class HashToken : ComplexToken {
+    public enum TypeFlag { Unrestricted, Id }
+
+    public TypeFlag type = TypeFlag.Unrestricted;
+
+    public HashToken(string codePoints, TypeFlag typeFlag) : base(codePoints, Tokens.hashToken) {
+        type = typeFlag;
+    }
+}
+
+public class NumberToken : ComplexToken {
+    public enum TypeFlag { Integrer, Number}
+    public TypeFlag type = Integrer;
+    public float value;
+
+    public NumberToken(string codePoints, float value, TypeFlag typeFlag) : base(codePoints, Tokens.numberToken) {
+        this.value = value;
+        type = typeFlag;
+    }
+}
+
+public class DimensionToken : ComplexToken {
+    public enum TypeFlag { Integrer, Number}
+    public TypeFlag type = Integrer;
+    public float value;
+    public string unit;
+
+    public DimensionToken(string codePoints, float value, TypeFlag typeFlag, string unit) 
+        : base(codePoints, Tokens.dimensionToken) 
+    {
+        this.value = value;
+        type = typeFlag;
+        this.unit = unit;
+    }
+}
+
+public class PercentToken : ComplexToken {
+    public float value;
+
+    public PercentToken(string codePoints, float value) : base(codePoints, Tokens.percentToken) {
+        this.value = value;
+    }
+}
+
+public class UnicodeRangeToken : Token {
+    public int start;
+    public int end;
+    
+    public UnicodeRangeToken(char codePoint, int start, int end) : base(codePoint, Tokens.unicodeRangeToken) {
+        this.start = start;
+        this.end = end;
+    }
+}
